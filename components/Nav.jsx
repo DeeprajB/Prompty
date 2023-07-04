3b2965a673be28aa36cfaab01e200021477cae74
DeeprajB <deeprajb012@gmail.com>
2023-07-04 14:41:57 +0530

    Completed Create Prompt FE.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
             {session?.user?
             (
                 <div className="flex gap-3 md:gap-5">
-                    <Link href="/create-prompt" className="black_btn">Create Post</Link>
+                    <Link href="/create-prompt" className="black_btn">Create Prompt</Link>
                     <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
                     <Link href="/profile">
                         <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="Profile-Icon" />
