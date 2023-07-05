d3341782277e5d741d60d4f650cc6d02905286c5
DeeprajB <deeprajb012@gmail.com>
2023-07-05 15:48:47 +0530

    Updated Cosmetics to Dark Mode.

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,14 +4,14 @@ const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
         <h1 className="head_text text-left">
-            <span className="blue_gradient">{type} Prompt</span>
+            <span className="purple_gradient">{type} Prompt</span>
         </h1>
         <p className="desc text-left max-w-md">
             {type} & share amazing prompts with the world, and let your imagination run wild with any AI-powered platform.
         </p>
         <form onSubmit={handleSubmit} className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
             <label>
-                <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
+                <span className="font-satoshi font-semibold text-base text-gray-300">Your AI Prompt</span>
                 <textarea
                     value={post.prompt}
                     onChange={(e) => setPost({ ...post, prompt: e.target.value})}
@@ -21,7 +21,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
                 />
             </label>
             <label>
-                <span className="font-satoshi font-semibold text-base text-gray-700">
+                <span className="font-satoshi font-semibold text-base text-gray-300">
                     Tag <span className="font-normal">(#product, #webdev, #idea)</span>
                 </span>
                 <input
@@ -33,13 +33,13 @@ const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
                 />
             </label>
             <div className="flex-end mx-3 mb-5 gap-4">
-                <Link href="/" className="text-gray-500 text-sm">
+                <Link href="/" className="text-gray-300 text-sm">
                     Cancel
                 </Link>
                 <button
                     type="submit"
                     disabled={submitting}
-                    className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+                    className="px-5 py-1.5 text-sm bg-violet-500 rounded-full text-white"
                 >
                     {submitting? `${type}...` : type}
                 </button>
