77c5652a895192aadb88739debc299e8782192a5
DeeprajB <deeprajb012@gmail.com>
2023-07-05 08:44:33 +0530

    Completed Profile BE & Minor PromptCard FE Patch.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -52,7 +52,7 @@ const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete }) => {
         >
             {prompt.tag}
         </p>
-        {session?.user.id === postMessage.creator._id && pathName === '/profile' && (
+        {session?.user.id === prompt.creator._id && pathName === '/profile' && (
             <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
                 <p
                     className="font-inter text-sm green_gradient cursor-pointer"
